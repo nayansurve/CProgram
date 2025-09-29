44d06e47c277172f1e2af97839f9e51bcce7122b
nayansurve <survenayan870@gmail.com>
2025-09-29 09:44:35 +0530

    Update

--- char_to_num.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int ch;
-    printf("Enter a character:");
-    scanf("%c",&ch);
-    printf("%d",ch);
-}
--- float.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float a=8.6;
-    float b=2.2;
-    float result=a+b;
-
-    printf("Result:%.2f",result);
-}
