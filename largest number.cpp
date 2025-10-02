ebd32f4a6e896f1a2db9f9aaa58be97c97126ad9
Nayan Bhaskar Surve <survenayan870@gmail.com>
2025-10-02 11:13:08 +0530

    Add files via upload

---
+++ largest number.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("enter a number");
+	scanf("%d %d %d", &a,&b,&c);
+	if(a>=b && a>=c){
+		printf("%d is the largest number.",a);
+	}
+	else if("b>=a && b>=C"){
+		printf("%d is the largest number.",b);
+	}
+	else("c>a && c>b "){
+		printf("%d is the largest number.",c);
+	}
+return 0;	
+	
+}
+
