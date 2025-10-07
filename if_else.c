3315454f22f6d4c06f6964478491ddf61ca35120
nayansurve <survenayan870@gmail.com>
2025-10-07 21:22:51 +0530

    if_else

---
+++ if_else.c
@@ -0,0 +1,15 @@
+main()
+{
+  int age;
+
+    printf("Enter your age:");
+    scanf("%d",&age);
+
+    if(age<18){
+        printf("You are not eligible for voting");
+    }
+    else{
+        printf("You are eligible for voting");
+    }
+
+}
