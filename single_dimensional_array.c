052372d3cc1a54aea7f8e4cff3a59e454ec1b8be
nayansurve <survenayan870@gmail.com>
2025-10-09 19:15:50 +0530

    basiccode

---
+++ single_dimensional_array.c
@@ -0,0 +1,18 @@
+main()
+{
+  int a[2];
+  printf("Enter 2 numbers:\n");
+
+  for(int i=0;i<2;i++)
+  {
+
+      scanf("%d",&a[i]);
+  }
+  printf("Display numbers");
+
+  for(int i=0;i<2;i++)
+  {
+      printf("\n %d",a[i]);
+  }
+
+}
