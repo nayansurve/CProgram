14535ee7779bd420abbfd708ff4eda5cabae5285
nayansurve <survenayan870@gmail.com>
2025-10-09 19:13:52 +0530

    Nestedloop

---
+++ nested_if.c
@@ -0,0 +1,15 @@
+main()
+{
+    int a=2;
+    int b=5;
+    printf("\n a:%d",a);
+    printf("\n b:%d",b);
+
+    if(a=2)
+        {
+        if(b==5)
+        {
+            printf("\n value of a is 2 and b is 5");
+        }
+    }
+}
